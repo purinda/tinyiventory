e866de1299945714339867dbdcba2618249ef1ba
Purinda Gunasekara <purinda@gmail.com>
2015-04-22 23:29:12 +1000

    Added form handling routine

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Medicine\Entity\Bundle\Entity\Item;
 
 class DefaultController extends Controller
@@ -11,7 +12,7 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="overview")
      */
-    public function overviewAction()
+    public function overviewAction(Request $request)
     {
         $form_data = [];
 
@@ -22,11 +23,27 @@ class DefaultController extends Controller
             ->add('send', 'submit', ['label' => 'Save'])
             ->getForm();
 
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $this->save($request);
+        }
+
         return $this->render(
             'default/overview.html.twig',
             [
                 'form' => $form->createView()
             ]
         );
+    }
+
+    /**
+     * Save the data found in the request
+     * @param  Request $request
+     * @return bool
+     */
+    public function save(Request $request)
+    {
+        dump($request);
     }
 }
