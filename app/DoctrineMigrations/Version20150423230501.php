1f725ae2dd21a4f51604d244a2beb7930aaa071b
Purinda Gunasekara <purinda@gmail.com>
2015-04-23 23:06:02 +1000

    Unique name

---
+++ app/DoctrineMigrations/Version20150423230501.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150423230501 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE item CHANGE added_date added_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1F1B251E5E237E06 ON item (name)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_1F1B251E5E237E06 ON item');
+        $this->addSql('ALTER TABLE item CHANGE added_date added_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+    }
+}
